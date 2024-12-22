f99df91e5bd0b73170a18e2deedd59bf26f3eccc
A113E <amg11amg2@gmail.com>
2024-12-22 12:40:23 -0500

    Fix: Configuración del servidor para Render

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Permite que el servidor sea accesible desde fuera del localhost
-    port: process.env.PORT || 5173 // Usa el puerto proporcionado por Render o un puerto por defecto
-  }
 })
-
