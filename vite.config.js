54c215ea6bdc9e64f661c94c8ecebc17ac2b6ae9
A113E <amg11amg2@gmail.com>
2024-12-22 12:55:39 -0500

    Fix: Configuración para compatibilidad local y Render

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: process.env.RENDER ? '0.0.0.0' : 'localhost', // Usa 'localhost' en desarrollo y '0.0.0.0' en Render
+    port: process.env.PORT || 5173 // Usa el puerto de Render o 5173 en local
+  }
 })
+
